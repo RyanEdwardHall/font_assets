00d8f956725c06f2c3f934a13ae3501fe49be4e8
Ryan Hall <hall.ryan@gmail.com>
2018-01-25 14:31:12 -0500

    Update railtie.rb
    I'm sub-classing and swapping out ActionDispatch::Static, so it's not actually in my stack.

--- lib/font_assets/railtie.rb
+++ lib/font_assets/railtie.rb
@@ -7,14 +7,7 @@ module FontAssets
     initializer "font_assets.configure_rails_initialization" do |app|
       config.font_assets.origin ||= "*"
       config.font_assets.options ||= { allow_ssl: true }
-
-      insert_target = if defined?(ActionDispatch::Static)
-        ActionDispatch::Static
-      else
-        Rack::Runtime
-      end
-
-      app.middleware.insert_before insert_target, FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
+      app.middleware.insert_before Rack::Runtime, FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
     end
   end
 end
